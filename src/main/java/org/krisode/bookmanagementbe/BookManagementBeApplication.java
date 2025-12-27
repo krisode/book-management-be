47b356c8b7c0c3e9adf5a5f62261c8e3521aed73
Huy Tran <trantrinhdanghuy1406@gmail.com>
2025-12-27 12:24:13 +0800

    Init commit

---
+++ src/main/java/org/krisode/bookmanagementbe/BookManagementBeApplication.java
@@ -0,0 +1,13 @@
+package org.krisode.bookmanagementbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagementBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/krisode/bookmanagementbe/BookManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.krisode.bookmanagementbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
